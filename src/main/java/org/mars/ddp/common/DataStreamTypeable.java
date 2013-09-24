7741588ef9d873d702dac2db1617697ea82949b7
mars <mars@zenith>
2013-09-24 22:35:03 +0200

    Factored constants (the return)

--- src/main/java/org/mars/ddp/common/DataStreamTypeable.java
+++ src/main/java/org/mars/ddp/common/DataStreamTypeable.java
@@ -2,6 +2,10 @@ package org.mars.ddp.common;
 
 
 public interface DataStreamTypeable {
+  public final static String TYPE_DM_MAIN = "DM";
+  public final static String TYPE_TEXT = "TS";
+  public final static String TYPE_SUBCODE = "DS";
+  
   public String getId();
   public String getType();
 }
