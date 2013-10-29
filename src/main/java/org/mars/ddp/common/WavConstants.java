c420a6ee312b00bd46ac40d4b461d8c59a351692
mars <mars@zenith>
2013-10-29 21:57:23 +0100

    Reorg constants

--- src/main/java/org/mars/ddp/common/WavConstants.java
+++ src/main/java/org/mars/ddp/common/WavConstants.java
@@ -1,8 +1,10 @@
 package org.mars.ddp.common;
 
 public interface WavConstants {
-  public static final String DATA_CHUNK_ID = "data";
-  public static final String FORMAT_CHUNK_ID = "fmt ";
-  public static final String WAVE_FORMAT_ID = "WAVE";
   public static final String RIFF_CHUNK_ID = "RIFF";
+  public static final String WAVE_FORMAT_ID = "WAVE";
+
+  public static final String FORMAT_CHUNK_ID = "fmt ";
+
+  public static final String DATA_CHUNK_ID = "data";
 }
