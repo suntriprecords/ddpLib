ce6d174204150a6397b33ff58164b9e29695f99a
mars <mars@zenith>
2014-07-02 17:23:01 +0200

    comment

--- src/main/java/org/mars/ddp/common/WavConstants.java
+++ src/main/java/org/mars/ddp/common/WavConstants.java
@@ -4,7 +4,7 @@ public interface WavConstants {
   public static final String RIFF_CHUNK_ID = "RIFF";
   public static final String WAVE_FORMAT_ID = "WAVE";
 
-  public static final String FORMAT_CHUNK_ID = "fmt ";
+  public static final String FORMAT_CHUNK_ID = "fmt "; //don't remove that space!
 
   public static final String DATA_CHUNK_ID = "data";
 }
